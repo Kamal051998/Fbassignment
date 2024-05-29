04aa82688945ca6cccfa168fd81dbe0b87974694
Kamal051998 <kamalpanwar192@gmail.com>
2024-05-29 11:40:04 +0530

    added dynamic checklist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-// import logo from './logo.svg';
-// import './App.css';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function App() {
- const [isChecked,setIsChecked]=useState()
+  const [checkedItems, setCheckedItems] = useState({});
 
- const checkChange=(e)=>{
- setIsChecked(e.target.checked)
+  const options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
 
- }
- console.log(isChecked&&isChecked)
+  const handleCheckChange = (e) => {
+    const { name, checked } = e.target;
+    setCheckedItems((prev) => ({
+      ...prev,
+      [name]: checked,
+    }));
+  };
 
   return (
-    <div className="">
-    <input onChange={checkChange} type='checkbox'  />
+    <div className="App">
+      <h1>Checklist</h1>
+      {options.map((option) => (
+        <div key={option}>
+          <input
+            type="checkbox"
+            name={option}
+            checked={checkedItems[option] || false}
+            onChange={handleCheckChange}
+          />
+          <label>{option}</label>
+        </div>
+      ))}
+      <div>
+        <h2>Selected Options:</h2>
+        <ul>
+          {Object.keys(checkedItems)
+            .filter((key) => checkedItems[key])
+            .map((key) => (
+              <li key={key}>{key}</li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
